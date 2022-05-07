611bf44a9b4104be7e5bc6ab580552bb8bc956ea
Nguyen Minh Hai <minhhai19062001@gmail.com>
2022-05-08 01:15:05 +0700

    add delete functionality

--- src/main/java/com/minhhai/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/minhhai/thymeleafdemo/controller/EmployeeController.java
@@ -5,13 +5,11 @@ import com.minhhai.thymeleafdemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/employees")
@@ -37,11 +35,29 @@ public class EmployeeController {
         return "employees/employee-form";
     }
 
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int id, Model model){
+        // get the employee from the service
+        Optional<Employee> employee = employeeService.findById(id);
+
+        // set employee as a model attribute to pre-populate the form
+        model.addAttribute("employee", employee);
+
+        // send over to our form
+        return "employees/employee-form";
+    }
+
     @PostMapping("/save")
     public String saveEmployee(@ModelAttribute("employee") Employee employee){
         // save the employee
         employeeService.save(employee);
         // use a redirect to prevent duplicate submissions
+        return "redirect:/employees/list";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId") int id){
+        employeeService.deleteById(id);
         return "redirect:/employees/list";
     }
 }
