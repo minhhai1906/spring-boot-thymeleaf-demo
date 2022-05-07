4e7187f686137e4f4881f36dfaf0979c8f414981
Nguyen Minh Hai <minhhai19062001@gmail.com>
2022-05-08 00:26:25 +0700

    add save employee

--- src/main/java/com/minhhai/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/minhhai/thymeleafdemo/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -14,7 +16,7 @@ import java.util.List;
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
-    private EmployeeService employeeService;
+    private final EmployeeService employeeService;
 
     @Autowired
     public EmployeeController(EmployeeService employeeService) {
@@ -25,7 +27,21 @@ public class EmployeeController {
     public String listEmployees(Model model){
         List<Employee> all = employeeService.findAll();
         model.addAttribute("employees", all);
-        return "list-employees";
+        return "employees/list-employees";
     }
 
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model){
+        var employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+        // save the employee
+        employeeService.save(employee);
+        // use a redirect to prevent duplicate submissions
+        return "redirect:/employees/list";
+    }
 }
