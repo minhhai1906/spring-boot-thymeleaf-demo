4ff20d141bf08eb632d0be3d5586525885ef4a6f
Nguyen Minh Hai <minhhai19062001@gmail.com>
2022-05-08 00:33:25 +0700

    sort employee by last name

--- src/main/java/com/minhhai/thymeleafdemo/dao/EmployeeRepository.java
+++ src/main/java/com/minhhai/thymeleafdemo/dao/EmployeeRepository.java
@@ -3,5 +3,8 @@ package com.minhhai.thymeleafdemo.dao;
 import com.minhhai.thymeleafdemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    List<Employee> findAllByOrderByLastNameAsc();
 }
